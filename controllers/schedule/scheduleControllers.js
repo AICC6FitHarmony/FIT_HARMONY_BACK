f61525ab8566e65e6b57d0b1d08321bd502257cc
NamYounDong <nyd6849@gmail.com>
2025-06-25 09:55:03 +0900

    schedule_controllers_modify

--- controllers/schedule/scheduleControllers.js
+++ controllers/schedule/scheduleControllers.js
@@ -10,14 +10,9 @@ const schedulerControllers = [
         type : 'post',
         callback : async ({request, params}) => {
             try {
-<<<<<<< HEAD
-                        
-=======
+
 
                 
->>>>>>> 0c3a95e705701243f855ce69865d03d3084e49f9
-
-
                 return { 
                     message: 'Login Success...',
                     success: true,
