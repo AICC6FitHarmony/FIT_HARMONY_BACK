8c616b4f3440c686009257de7d8bad62e9aedb4d
hyo91 <thgydnjs@gmail.com>
2025-07-03 15:22:41 +0900

    trainer

--- controllers/trainer/trainer.js
+++ controllers/trainer/trainer.js
@@ -8,31 +8,63 @@ const getTrainerList = async (req, res) => {
     
     `;
 
-    const trainerquery = `SELECT
-      u.user_id,
-      u.user_name,
-      u.gender,
-      g.gym,
-      g.gym_address,
-      MIN(p.price) AS min_price,
-      ROUND(AVG(r.rating), 2) AS rating,
-      COUNT(r.review_id) AS review_count
+    const trainerquery = `
+      SELECT
+        u.user_id,
+        u.user_name,
+        u.gender,
+        g.gym,
+        g.gym_address,
+        MIN(p.price) AS min_price,
+        ROUND(AVG(r.rating), 2) AS rating,
+        COUNT(r.review_id) AS review_count
       FROM "USER" u
-      LEFT OUTER JOIN products p ON u.user_id = p.user_id
-      LEFT OUTER JOIN gym g ON u.gym_id = g.gym_id
-      LEFT OUTER JOIN review r ON p.product_id = r.product_id
+
+      LEFT OUTER JOIN products p 
+      ON u.user_id = p.user_id
+
+      LEFT OUTER JOIN gym g 
+      ON u.gym_id = g.gym_id
+
+      LEFT OUTER JOIN review r 
+      ON p.product_id = r.product_id
+
       WHERE u.role = 'TRAINER'
+
       GROUP BY u.user_id, u.user_name, u.gender, g.gym, g.gym_address
+      `;
+
+    const trainerdetailquery = `
+      select
+	
+	      u.user_id,
+        u.user_name,
+        u.fit_history,
+	      u.introduction,
+	      u.file_id,
+        g.gym,
+        g.gym_address,
+        ROUND(AVG(r.rating), 2) AS rating,
+        COUNT(r.review_id) AS review_count
+
+      FROM "USER" u
+      LEFT OUTER JOIN gym g ON u.gym_id = g.gym_id
+      LEFT OUTER JOIN products p ON u.user_id = p.user_id
+      LEFT OUTER JOIN review r ON p.product_id = r.product_id
+      where u. role = 'TRAINER'
+      group by u.user_id, u.user_name, u.fit_history, u.introduction, u.file_id, u.gender, g.gym, g.gym_address 
       `;
 
     const trainerresult = await sendQuery(trainerquery);
     const trainerlistresult = await sendQuery(trainerlistquery);
+    const trainerdetailresult = await sendQuery(trainerdetailquery);
     // console.log(trainerresult);
 
     res.status(200).json({
       success: true,
       data: trainerresult,
       total: trainerlistresult,
+      detail: trainerdetailresult,
       message: '트레이너 목록 조회 성공',
     });
   } catch (error) {
