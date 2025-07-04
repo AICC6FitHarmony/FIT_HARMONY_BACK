fb2c4cd86370a35075d547ef93badcea425f12f5
NamYounDong <nyd6849@gmail.com>
2025-07-04 18:20:20 +0900

    chart_commit

--- controllers/schedule/gptModel.py
+++ controllers/schedule/gptModel.py
@@ -19,7 +19,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("D:/log/gptmodelLog.log", encoding="utf-8"),
+        # logging.FileHandler("D:/log/gptmodelLog.log", encoding="utf-8"),
         logging.StreamHandler()
     ]
 )
