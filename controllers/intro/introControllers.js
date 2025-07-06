46c98583e7ce19d79cbc42077f9f25e900a610f4
김준영 <1090jun@gmail.com>
2025-07-06 17:30:40 +0900

    인트로_커뮤니티

--- controllers/intro/introControllers.js
+++ controllers/intro/introControllers.js
@@ -10,14 +10,64 @@ const introControllers = [
     {
         url : '/',
         type : 'get',
-        callback : async () => {
+        callback : async ({request, params}) => {
             try {
 
                 const data = {
                     diet: [],
-                    community: [],
+                    communityLatest: [],
+                    communityHot: [],
                     trainer: [],
                 }
+                const introDietQuery = `SELECT diet_main_menu_name, 
+                                               COUNT(*) AS diet_count,
+                                              (SELECT file_id
+                                               FROM diet AS d2 
+                                               WHERE d2.diet_main_menu_name = d1.diet_main_menu_name
+                                               LIMIT 1) AS sample_file_id
+                                        FROM diet AS d1
+                                        GROUP BY diet_main_menu_name
+                                        LIMIT 10
+                                        `
+
+                const introCommunityLatestQuery = ` SELECT post_id, 
+                                                           title,
+                                                          (SELECT COUNT(*)
+                                                           FROM comment c 
+                                                    WHERE p.post_id = c.post_id) AS comment_count 
+                                                    FROM post p 
+                                                    ORDER BY created_time DESC 
+                                                    LIMIT 5
+                                                    `
+                const introCommunityHotQuery = `SELECT post_id, 
+                                                       title,
+                                                      (SELECT COUNT(*)
+                                                       FROM comment c 
+                                                       WHERE p.post_id = c.post_id) AS comment_count
+                                                FROM post p
+                                                ORDER BY comment_count DESC 
+                                                LIMIT 5
+                                                `
+                const introTrainerQuery =  `SELECT u.user_id, 
+                                                   u.file_id, 
+                                                   u.nick_name, 
+                                                   u.fit_goal, 
+                                                   COUNT(p.product_id) AS product_count
+                                            FROM "USER" u
+                                            LEFT JOIN PRODUCTS p ON u.user_id = p.user_id
+                                            WHERE u.role = 'TRAINER'
+                                            AND u.status = 'ACTIVE'
+                                            GROUP BY u.user_id, u.file_id, u.nick_name, u.fit_goal
+                                            ORDER BY product_count DESC
+                                            LIMIT 10
+                                            `
+
+                data.diet = await sendQuery(introDietQuery)
+                data.communityLatest = await sendQuery(introCommunityLatestQuery)
+                data.communityHot = await sendQuery(introCommunityHotQuery)
+                data.trainer = await sendQuery(introTrainerQuery)
+
+                console.log("introData" ,data);
 
                     return { 
                         message: 'success',
