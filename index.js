d464a83e6287ac058e4bf8567af0bb0236e71a55
NamYounDong <nyd6849@gmail.com>
2025-07-07 13:32:50 +0900

    common_slice_commit

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ app.get(
   passport.authenticate('google', { failureRedirect: '/login-fail'}),
   (request, response) => {
     // 세션 저장 후 프론트에서 인증 확인 가능
-    response.redirect(`${process.env.FRONT_DOMAIN}/auth/google/result`); //  동작 테스트 확인 필요
+    response.redirect(`${process.env.FRONT_DOMAIN}/`); //  동작 테스트 확인 필요
   }
 );
 
